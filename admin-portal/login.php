88acc3d59119257e583f27c591f0644cc9da9733
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-07-24 00:47:30 +0530

    login ui added

--- admin-portal/login.php
+++ admin-portal/login.php
@@ -32,26 +32,44 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+    <script src="js/login.js"></script>
+
 </head>
 
 <body>
-    <h1>Login as Admin</h1>
+
+    <hgroup>
+        <h1>Admin Portal</h1>
+        <h3>Login as an admin</h3>
+    </hgroup>
     <form action="login.php" method="post" autocomplete="off">
-        <?php 
-        if(isset($_GET['logout'])){
-            echo '<p class="infomsg"> You have successfuly logout from the system.</p>';
+        <?php
+        if (isset($_GET['logout'])) {
+            echo '<p class="infomsg"> You have successfuly logout from the system</p>';
         }
-         ?>
-        Email: <input type="email" name="admin_mail" maxlength="50" autocomplete="false" required>
-        <br>
-        Password: <input type="password" name="admin_pw" maxlength="20" autocomplete="false" required>
-        <br>
-        <input type="submit" name="submit" value="login">
+        ?>
+        <div class="group">
+            <input type="email" name="admin_mail" maxlength="50" autocomplete="false" required>
+            <span class="highlight"></span><span class="bar"></span>
+            <label>Email</label>
+        </div>
+        <div class="group">
+            <input type="password" name="admin_pw" maxlength="20" autocomplete="false" required>
+            <span class="highlight"></span><span class="bar"></span>
+            <label>Password</label>
+        </div>
+        <button type="submit" name="submit" class="button buttonBlue"> Login
+            <div class="ripples buttonRipples">
+            <span class="ripplesCircle"></span></div>
+        </button>
     </form>
+
 </body>
 
 </html>
