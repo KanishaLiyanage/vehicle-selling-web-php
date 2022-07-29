aa52db1efe831a62e2e4059b5303e5a3b69e5769
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-07-29 19:38:41 +0530

    minor changes

--- website/h.php
+++ website/h.php
@@ -13,50 +13,6 @@
 
 <body>
 
-<!-- Profile Page , Modal -->
-<div class="page-header"></div>
-<div class="profile-container">
-  <div class="row">
-  <div class="col-md-4">
-    <img src="https://image.ibb.co/jxGOma/image.jpg" class="img-responsive" width="100" height="100" />
-  </div>
-  <div class="col-md-8 profile-info">
-    <h4 class="user-name">Abd Alrahman Fani</h4>
-    <h5 class="user-mail"><i>email@dmain.com</i></h5>
-    <h5 class="user-company">SYRIAN ARTS</h5>
-  </div>
-</div>
-  <br/><br/>
-  <div class="row more-info">
-  <div class="col-md-6">
-    <h5><b>Work</b></h5>
-    <p>Full-Stack Developer</p>
-  </div>
-    <div class="col-md-6">
-      <h5><b>Made In</b></h5>
-    <p>Syrian Arab Republic</p>
-  </div>
-  </div>
-   <div class="row more-info">
-  <div class="col-md-6">
-    <h5><b>Relationship</b></h5>
-    <p>Single :(</p> <!-- </3 :( -->
-  </div>
-    <div class="col-md-6">
-      <h5><b>Phone Number</b></h5>
-    <p>+124 1234 52 145</p>
-  </div>
-  </div>
-  <br/>
-  <div class="row">
-    <div class="todo-links"><a><i class="fa fa-pencil-square-o"></i> Edit Info</a>
-    </div>
-    <div class="todo-links"><a><i class="fa fa-frown-o"></i> Logout</a>
-    </div>
-
-  </div>
-</div>
-
 </body>
 
 
